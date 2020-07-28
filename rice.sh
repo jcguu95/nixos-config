140763659e44f997c7e4b76ef2417a9694986789
Jin <jcguu95@gmail.com>
2020-07-28 12:42:36 -0500

    Fix fatal part in rice.sh

--- rice.sh
+++ rice.sh
@@ -1,18 +1,20 @@
 # Run this as root
 
-## System Config
+## System Config TODO this part should be separated
 git clone https://github.com/jcguu95/nixos-config
 cd nixos-config
 
 mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.bak
-cp ./configuration.nix /etc/nixos.configuration.nix
+cp ./configuration.nix /etc/nixos/configuration.nix
+
+##################
 
 ## Git Config
 git config --global user.name "Jin"
 git config --global user.email "jcguu95@gmail.com"
 
 ## .xinitrc
-echo "ssh-agent xmonad" >> "$/home/jin/.xinitrc"
+echo "ssh-agent xmonad" >> "/home/jin/.xinitrc"
 
 ## TODO xmonad & xmobar
 
