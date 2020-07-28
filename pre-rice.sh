aceafa8114619add780cb6eac9a00f13ca570895
Jin <jcguu95@gmail.com>
2020-07-28 12:53:13 -0500

    Separate pre-rice from rice, and did some extra edit.

--- pre-rice.sh
+++ pre-rice.sh
@@ -1,4 +1,14 @@
-# Run this script right after installing nixOS.
-# It will inject configuration.nix and auto rebuild.
+# Run this as root
 
-# TODO
+## System Config 
+nix-env -iA nixos.git
+git clone https://github.com/jcguu95/nixos-config
+nix-env -e nixos.git
+
+cd nixos-config
+tmpDir=tmp-$(date -Is); mkdir $tmpDir
+mv /etc/nixos/configuration.nix ./$tmpDir/configuration.nix.bak
+cp ./configuration.nix /etc/nixos/configuration.nix
+nixos-rebuild switch
+
+exit
--- rice.sh
+++ rice.sh
@@ -1,14 +1,3 @@
-# Run this as root
-
-## System Config TODO this part should be separated
-git clone https://github.com/jcguu95/nixos-config
-cd nixos-config
-
-mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.bak
-cp ./configuration.nix /etc/nixos/configuration.nix
-
-##################
-
 ## Git Config
 git config --global user.name "Jin"
 git config --global user.email "jcguu95@gmail.com"
