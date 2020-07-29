f71b7af2b9a6f6f26b6118223e4d5209ef780147
Jin <jcguu95@gmail.com>
2020-07-29 09:19:42 -0500

    Made (pre)-rice.sh more sane.

--- pre-rice.sh
+++ pre-rice.sh
@@ -1,10 +1,5 @@
 # Run this as root after rebooting from a clean installation of nixOS
 
-nix-env -iA nixos.git
-git clone https://github.com/jcguu95/nixos-config
-nix-env -e nixos.git
-
-cd nixos-config
 tmpDir=tmp-$(date -Is); mkdir $tmpDir;
 mv /etc/nixos/configuration.nix ./$tmpDir/configuration.nix.bak
 cp ./configuration.nix /etc/nixos/configuration.nix
--- rice.sh
+++ rice.sh
@@ -1,17 +1,29 @@
 # Should be run after a clean installation of nixOS and `pre-rice.sh`.
 
 ## Git Config
+echo "Configuring git.."
 git config --global user.name "Jin"
 git config --global user.email "jcguu95@gmail.com"
+echo "Done."
 
 ## X11 Config
+echo "Configuring X11.."
 echo "ssh-agent xmonad" >> "/home/jin/.xinitrc"
+echo "Done."
 
 ## xmonad & xmobar
 ### xmonad
+echo "Configuring xmonad.."
 mv $HOME/.xmonad $HOME/.xmonad_bak
 ln -sf $(pwd)/.xmonad $HOME/.xmonad
 xmonad --recompile
+echo "Done."
 ### xmobar
+echo "Configuring xmobar.."
 ln -sf $(pwd)/xmobarrc $HOME/.xmobarrc
+echo "Done."
+
+echo "All done. Exit in 5 seconds.."; sleep 5;
+
+exit
 
