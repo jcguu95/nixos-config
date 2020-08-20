6ef14cb12379fd745e258fa10e2f1b36dc83d8aa
jcguu95 <jcguu95@gmail.com>
2020-08-20 06:58:06 -0500

    Make pre-rice talk.

--- pre-rice.sh
+++ pre-rice.sh
@@ -1,7 +1,14 @@
 # Run this as root after rebooting from a clean installation of nixOS
 
+echo "Backing up configuration.nix in the same directory /etc/nixos/ .."
 mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.bak.$(date -Is)
+
+echo "Symlinking our configuration.nix to /etc/nixos .."
 ln -sf $(pwd)/configuration.nix /etc/nixos/configuration.nix
+
+echo "Rebuilding.."
 nixos-rebuild switch
+
+echo "DONE!"
 
 exit
