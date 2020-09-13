7a50f7cbc0c9cc0f98d0edb8404cdd58861907c7
jin <jcguu95@gmail.com>
2020-09-13 07:08:36 -0500

    minor fix

--- rice-for-arch.sh
+++ rice-for-arch.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 # this script must be run in this directory
 
-ln -sf $(pwd)/tilde/.xmonad ~/.xmonad
-
+## .config
 link() {
   echo "soft linking $1 to $2"
   ln -sf $1 $2
@@ -16,8 +15,7 @@ for entry in $(pwd)/tilde/.config/*; do
   link "$entry" ~/.config/ || complain "$entry"
 done
 
-
-#ln -sf $(pwd)/tilde/.config/xmobar ~/.config/xmobar
-#ln -sf $(pwd)/tilde/.config/alacritty.yml ~/.config/alacritty.yml
+## Others
+ln -sf $(pwd)/tilde/.xmonad ~/.xmonad
 
 exit
